c4fc4bdba2532619fb8803943ea6082e7b3138f7
alushya <alushyarahma@gmail.com>
2025-12-04 13:50:27 +0700

    sushitei

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/dashboard', function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 //     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+// }); 
+
+// awa
 
 require __DIR__.'/auth.php';
